ab8752c960065cd5293e2bfa562c7fe7fdc5b078
vagrant <vagrant@localhost.localdomain>
2020-08-03 04:49:00 +0000

    action

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,2 +1,20 @@
 class Admin::ProductsController < ApplicationController
+
+	def index
+	end
+
+	def new
+	end
+
+	def create
+	end
+
+	def update
+	end
+
+	def show
+	end
+
+	def edit
+	end
 end
--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,2 +1,14 @@
 class CustomersController < ApplicationController
+	def show
+	end
+
+	def resign
+	end
+
+	def edit
+	end
+
+	def update
+	end
+
 end
